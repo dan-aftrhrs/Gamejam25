9e360ce3d6514464689a86aea6b67a6c469f9510
dan-aftrhrs <dan.aftrhrs@gmail.com>
2025-12-01 20:50:45 +1100

    Changed move direction
    No longer hard coded the direction, and now it's setup so that the direction of movement can be dictated by the spawn point.

--- gamejam-25/Scenes/sushi.gd
+++ gamejam-25/Scenes/sushi.gd
@@ -1,5 +1,6 @@
 extends Area2D
 @export var speed: float = 400.0
+@export var move_direction: Vector2 = Vector2.LEFT
 
 func _process(delta: float) -> void:
-	position.x += speed * delta * -1
+	position.x += speed * delta * move_direction.x
